784d7aee2853f2c24eff51ae1d9624a6b0ebc8ec
Lisa Nguyen <hello@lisanguyen.me>
2016-12-15 15:57:49 +0000

    added tuples

---
+++ TalkingTuples.hs
@@ -0,0 +1,10 @@
+--returns the first element in tuple
+orderUp = fst ("burger", "fries")
+
+--returns the second element in tuple
+lastOrder = snd ("burger", "fries")
+
+--playing with zip
+matchNumbers = zip [1..5]["one", "two", "three", "four", "five"]
+
+--finding the numbers that make up a right-angle triangle with a perimeter of 24
